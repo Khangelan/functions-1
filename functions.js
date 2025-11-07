f68f05fe44b8269371632ff7517431ed1714837b
Khangelani Mhlakane <kmhlakane@gmail.com>
2025-11-07 14:12:14 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,19 @@
+function muitiply(num1, num2, num3){
+    return num1 * num2 * num3;
+}
+console.log(muitiply(87 ,56 , 98))
+
+function convertToSeconds(minutes){
+    return minutes*60;
+}
+console.log(convertToSeconds(60))
+
+function  fahrenheitToCelsius(fahrenheit){
+   return (fahrenheit - 32) * 5 / 9;
+}
+console.log(fahrenheitToCelsius(60))
+function reverseaString(str){
+    return str.split("").reverse().join("")
+}console.log(reverseaString("hello"))
+
+//i dont know how to do the rest of he quesitions im going to have to put time in them**
